1df67a6472ed39bc9ef447b2865978f8e11cdb42
nawad1 <nawad1@chicagobooth.edu>
2024-07-24 10:15:40 +0000

    base functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/users",{:controller => "users", :action => "index"})
+  get("/",{:controller => "users", :action => "index"})
+  get("/users/:path_username",{:controller => "users", :action => "show"})
 end
