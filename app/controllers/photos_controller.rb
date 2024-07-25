ac4a7a0973a04c5bce9938b531cbb7c015564d1f
nawad1 <nawad1@chicagobooth.edu>
2024-07-25 07:25:09 +0000

    delete photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,18 @@ class PhotosController < ApplicationController
   @list_of_photos = matching_photos.order ({:created_at => :desc})
   render({:template=> "photo_templates/index"})
  end
+ def show
+  url_id = params.fetch("path_id")
+  matching_photos = Photo.where({:id => url_id})
+  @the_photo = matching_photos.at(0)
+  render ({:template=> "photo_templates/show"})
+ end
+ def delete
+  the_id = params.fetch("path_id")
+  matching_photos = Photo.where({:id => the_id })
+  the_photo = matching_photos.at(0)
+  the_photo.destroy
+  redirect_to("/photos")
+
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/",{:controller => "users", :action => "index"})
   get("/users/:path_username",{:controller => "users", :action => "show"})
   get("/photos",{:controller => "photos", :action => "index"})
+  get("/photos/:path_id",{:controller => "photos", :action => "show"})
+  get("/delete_photo/:path_id",{:controller => "photos", :action => "delete"})
 end
