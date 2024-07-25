fa76c521c098201b376f1c26290de3d7511e069a
nawad1 <nawad1@chicagobooth.edu>
2024-07-25 08:36:37 +0000

    add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,16 @@ class UsersController < ApplicationController
     render ({:template => "user_templates/show"})
     end
   end
+  def create
+    input_username = params.fetch("input_username")
+
+  
+    a_new_user = User.new
+    a_new_user.username = input_username
+
+  
+    a_new_user.save
+  
+    redirect_to("/users/" + a_new_user.username)
+  end
 end
